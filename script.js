965d488800335c023dacde9c47749b8e72241212
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2018-11-05 14:13:54 -0700

    updates

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+
 var button = document.getElementById("enter");
 var input = document.getElementById('user-input');
+var li1 = document.getElementById("li1");
 var ul = document.querySelector("ul");
+var  li = document.querySelector("li");
+var liAll = document.querySelectorAll("li");
+var h2test = document.getElementById("h2test");
+
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
+	let span = document.createElement("span");
+    li.appendChild(span);
+    span.classList.add("off-set");
+    let deleteButton = document.createElement("button");
+    span.after(deleteButton);
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    li.classList.add("completed");
+    deleteButton.classList.add("delete-after");
 	input.value = "";
 }
 
@@ -23,6 +37,25 @@ function addListAfterKeyPress(event) {
 		createListElement();
 		}
 }
+
+function lineThrough() {
+	liAll.forEach(function(elem) {
+		elem.addEventListener("click", elem.classList.toggle("done"))
+	})
+	
+}
+
 button.addEventListener("click", addListAfterClick);
 	
 input.addEventListener("keypress", addListAfterKeyPress);
+
+document.addEventListener("click", function(event) {
+	if (event.target.matches(".completed")) {
+		event.target.classList.toggle("done");	}
+	if(event.target.matches(".delete")) {
+		event.target.parentElement.parentElement.remove();
+	}
+	if(event.target.matches(".delete-after")) {
+		event.target.parentElement.remove();
+	}
+},false);
